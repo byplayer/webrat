59f083948c2d88b4752a317cf5f7cddb61f3f6bf
byplayer <byplayer100@gmail.com>
2010-06-03 01:57:01 +0900

    refactor host name and port

--- spec/integration/mechanize/spec/mechanize_spec.rb
+++ spec/integration/mechanize/spec/mechanize_spec.rb
@@ -1,20 +1,21 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
 describe "Webrat's Mechanize mode" do
+  TEST_HOST = "localhost:9292"
+
   it "should work" do
-    response = visit("http://localhost:9292/")
+    response = visit("http://#{TEST_HOST}/")
     response.should contain("Hello World")
   end
 
   it "should follow redirects" do
-    response = visit("http://localhost:9292/internal_redirect")
+    response = visit("http://#{TEST_HOST}/internal_redirect")
     response.should contain("Redirected")
+    response.uri.to_s.should == "http://#{TEST_HOST}/redirected"
   end
 
-  it "should follow links"
-
   it "should submit forms" do
-    visit "http://localhost:9292/form"
+    visit "http://#{TEST_HOST}/form"
     fill_in "Email", :with => "albert@example.com"
     response = click_button "Add"
 
